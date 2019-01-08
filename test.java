6f6f47ba005fb4adcf1515f3602c144841baa2eb
bastienbrj <brajon.bastien@gmail.com>
2019-01-08 11:35:34 +0100

    b

--- test.java
+++ test.java
@@ -2,7 +2,7 @@ public class test {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello Bastien and Kevin");
+        System.out.println("Hello Bastien");
     }
 
 }
