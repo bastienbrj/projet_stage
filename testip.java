005d7d91b01b70e674e648d223d3f98120c3697c
bastienbrj <brajon.bastien@gmail.com>
2019-01-08 14:10:11 +0100

    test

---
+++ testip.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class testip {
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        String ipAddress = "127.0.0.1";
+        InetAddress inet = InetAddress.getByName(ipAddress);
+    
+        System.out.println("Sending Ping Request to " + ipAddress);
+        System.out.println(inet.isReachable(5000) ? "Host is reachable" : "Host is NOT reachable");
+    }
+}
+
