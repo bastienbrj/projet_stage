e9bf948d33dfaf114cac5a1eab41b458d91b1bbd
bastienbrj <brajon.bastien@gmail.com>
2019-01-09 10:44:13 +0100

    3

--- testip.java
+++ testip.java
@@ -4,7 +4,7 @@ import java.net.UnknownHostException;
 
 public class testip {
     public static void main(String[] args) throws UnknownHostException, IOException {
-        String ipAddress = "127.0.0.1";
+        String ipAddress = "10.34.8.24";
         InetAddress inet = InetAddress.getByName(ipAddress);
     
         System.out.println("Sending Ping Request to " + ipAddress);
